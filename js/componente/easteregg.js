9ddad15cb4be45af86e832124b5e3f2bcb42061a
Ricardo Kurdt <ricardo.kurdt@gmail.com>
2020-03-30 18:19:18 -0300

    entrega faltando carroussel, easter egg e paralax

---
+++ js/componente/easteregg.js
@@ -0,0 +1,17 @@
+(() => {
+    const $separators = document.querySelectorAll('.separator-dino');
+    let counter = 0;
+    const goalShowKey = $separators.length;
+  
+    $separators.forEach(($separator) => {
+      $separator.addEventListener('click', function() {
+        !this.classList.contains('-clicked') && counter++;
+        this.classList.add('-clicked');
+        counter === goalShowKey && showKey();
+      });
+    });
+  
+    function showKey() {
+      alert("Mostra key animada!!!")
+    }
+  })();
